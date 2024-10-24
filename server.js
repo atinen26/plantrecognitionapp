7df245326d793fba4615568ead458d1d2467be05
atinen26 <kobe_atinen26@yahoo.com>
2024-10-25 04:19:52 +0800

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json({ limit: '10mb' }));
 	
 // Example using Express.js
    app.use((req, res, next) => {
-     res.header('Access-Control-Allow-Origin', 'http://localhost:8000'); // Allow requests from your frontend's origin
+     res.header('Access-Control-Allow-Origin', '*'); // Allow requests from your frontend's origin
      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
      next();
